77690efe4ebecc3f0aafcf6c5b95c37e386e5a06
ROBERTO FERNANDO TOAPANTA VALENCIA <holidaysskywars@gmail.com>
2026-09-11 02:18:02 -0500

    chore: configure eslint and prettier

--- tests/devops.test.js
+++ tests/devops.test.js
@@ -3,14 +3,12 @@ const app = require('../src/app');
 
 describe('DevOps API', () => {
   test('POST /DevOps without API key returns 401', async () => {
-    const response = await request(app)
-      .post('/DevOps')
-      .send({
-        message: 'This is a test',
-        to: 'Juan Perez',
-        from: 'Rita Asturia',
-        timeToLifeSec: 45,
-      });
+    const response = await request(app).post('/DevOps').send({
+      message: 'This is a test',
+      to: 'Juan Perez',
+      from: 'Rita Asturia',
+      timeToLifeSec: 45,
+    });
 
     expect(response.status).toBe(401);
   });
