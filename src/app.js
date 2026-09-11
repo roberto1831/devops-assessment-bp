87811b43f053bdea85474e7c12cf916308b6013e
ROBERTO FERNANDO TOAPANTA VALENCIA <holidaysskywars@gmail.com>
2026-09-11 02:40:08 -0500

    feat: add token endpoint

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { signToken } = require('./jwt');
 
 const app = express();
 
@@ -17,6 +18,18 @@ app.post('/DevOps', (req, res) => {
   }
 
   return res.status(200).send();
+});
+
+app.post('/token', (req, res) => {
+  const apiKey = req.header('X-Parse-REST-API-Key');
+
+  if (!apiKey) {
+    return res.status(401).send();
+  }
+
+  return res.status(200).json({
+    token: signToken(),
+  });
 });
 
 module.exports = app;
