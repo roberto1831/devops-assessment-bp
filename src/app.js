91e9655ec7e8a8675875b8def1b8bc170980c512
ROBERTO FERNANDO TOAPANTA VALENCIA <holidaysskywars@gmail.com>
2026-09-11 01:53:47 -0500

    feat: validate missing jwt

--- src/app.js
+++ src/app.js
@@ -6,8 +6,13 @@ app.use(express.json());
 
 app.post('/DevOps', (req, res) => {
   const apiKey = req.header('X-Parse-REST-API-Key');
+  const jwt = req.header('X-JWT-KWY');
 
   if (!apiKey) {
+    return res.status(401).send();
+  }
+
+  if (!jwt) {
     return res.status(401).send();
   }
 
