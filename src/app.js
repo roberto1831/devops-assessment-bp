e9a3ba8e56483fde51a4c514ca4cb25a919bdd2e
ROBERTO FERNANDO TOAPANTA VALENCIA <holidaysskywars@gmail.com>
2026-09-11 01:41:03 -0500

    feat: validate missing api key

--- src/app.js
+++ src/app.js
@@ -4,4 +4,14 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/DevOps', (req, res) => {
+  const apiKey = req.header('X-Parse-REST-API-Key');
+
+  if (!apiKey) {
+    return res.status(401).send();
+  }
+
+  return res.status(200).send();
+});
+
 module.exports = app;
